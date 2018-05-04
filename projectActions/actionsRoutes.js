0f840a0e2928429efe8685fa03c4ea314f06e505
Harrison Brock <harrison@harrisonbrock.com>
2018-05-04 14:49:28 -0400

    Create new action done

--- projectActions/actionsRoutes.js
+++ projectActions/actionsRoutes.js
@@ -26,4 +26,24 @@ router.get('/:id', (req, res) => {
         .catch(() => res.status(500).send({ error: 'Error fetching actionss' }))
 });
 
+router.post('/', (req, res) => {
+
+    const action = req.body;
+
+    if (!action.project_id || !action.description) {
+        res.status(400).send({error:'Project Id and description are need'})
+    }
+    else if (action.description.length > 128) {
+        res.status(400).send({error: 'Description cannot be over 128 chars'})
+    }
+    else {
+        actionsDb.insert(action)
+            .then(response => {
+                res.status(202).send(response);
+            })
+            .catch(error => {
+                res.status(500).send({error: 'Added Project to database'})
+            })
+    }
+});
 module.exports = router;
